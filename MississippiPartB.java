851e17ab3ede371a1288527639692cdcdd1dc4a0
namedrandom04 <zespychikenz@gmail.com>
2018-09-19 10:44:00 -0400

    MississippiPartB

---
+++ MississippiPartB.java
@@ -0,0 +1,22 @@
+public class MississippiPartB{
+  public static void main(String[] args) {
+    lLetter();
+    eLetter();
+    tLetter();
+    tLetter();
+    eLetter();
+    rLetter();
+  }
+  public static void lLetter(){
+    System.out.println(" __ \n|  |\n|  |\n|  |\n|  `----.\n|_______|");
+  }
+  public static void eLetter(){
+    System.out.println(" _______\n|   ____|\n|  |__  \n|   __|  \n|  |____\n|_______|");
+  }
+  public static void tLetter(){
+    System.out.println(".___________.\n|           |\n`---|  |----`\n    |  |    \n    |  |    \n    |__|    ");
+  }
+  public static void rLetter(){
+    System.out.println(".______    \n|   _  \\    \n|  |_)  |    \n|      / \n|  |\\  \\----.\n| _| `._____|");
+  }
+}
