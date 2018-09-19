ec2452af87a563edbc7c30f83dd6227635f5a04f
namedrandom04 <zespychikenz@gmail.com>
2018-09-19 10:27:41 -0400

    Shining lab 4

---
+++ Shining.java
@@ -0,0 +1,27 @@
+public class Shining {
+    public static void main(String[] args) {
+      text();
+      text();
+      text();
+      text();
+      text();
+      text();
+      text();
+      text();
+      text();
+      text();
+    }
+    public static void text() {
+      System.out.println("All work and no play makes Jack a dull boy.");
+      System.out.println("All work and no play makes Jack a dull boy.");
+      System.out.println("All work and no play makes Jack a dull boy.");
+      System.out.println("All work and no play makes Jack a dull boy.");
+      System.out.println("All work and no play makes Jack a dull boy.");
+      System.out.println("All work and no play makes Jack a dull boy.");
+      System.out.println("All work and no play makes Jack a dull boy.");
+      System.out.println("All work and no play makes Jack a dull boy.");
+      System.out.println("All work and no play makes Jack a dull boy.");
+      System.out.println("All work and no play makes Jack a dull boy.");
+    }
+
+}
