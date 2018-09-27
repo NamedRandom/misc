5fc6d6117430b10e14dbc2ad14cf583d10a1edfa
namedrandom04 <zespychikenz@gmail.com>
2018-09-27 13:32:57 -0400

    finished primitvedata

--- PrimitiveDataTypes.java
+++ PrimitiveDataTypes.java
@@ -1,13 +1,30 @@
+// mert albayrak PrimitiveDataTypes lab
+//9-27-2018
 public class PrimitiveDataTypes {
   public static void main(String[] args) {
     int larryNum,moeNum,curlyNum;
     larryNum = 2;
     moeNum = 4;
     curlyNum = 6;
-    char letter='X'
-    bool test=true
+    char letter='X';
+    boolean test=true;
     double abbottNum= 1.5;
     double costelloNum = 2.63;
     System.out.println("Lab 2\tMert Albayrak");
+    System.out.println("----\t ---------");
+    System.out.println();
+    System.out.println("Larry's value is "+larryNum);
+    System.out.println("Moe's value is "+moeNum);
+    System.out.println("Curly's value is "+curlyNum);
+    System.out.println("letter's value is "+letter);
+    System.out.println("test's value is "+test);
+    System.out.println("abbot's value is "+abbottNum);
+    System.out.println("costello's value is "+costelloNum);
+    System.out.println();
+    System.out.println("the sum of larry moe and curly's values are "+(larryNum+moeNum+curlyNum));
+    System.out.println();
+    System.out.println("the sum of abbot and costello are "+(abbottNum+costelloNum));
+    System.out.println();
+    System.out.println(""+larryNum+moeNum+curlyNum+letter+test+abbottNum+costelloNum);
   }
 }
