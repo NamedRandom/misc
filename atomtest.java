51919554f07af95f88e73903d0a02ef9e6e6b651
namedrandom <mertalbayrak@gmail.com>
2018-09-27 21:02:57 -0400

    emptied atomtest

--- atomtest.java
+++ atomtest.java
@@ -1,27 +1,6 @@
 public class atomtest{
   public static void main(String[] args) {
-    mLetter();
-    iLetter();
-    sLetter();
-    sLetter();
-    iLetter();
-    sLetter();
-    sLetter();
-    iLetter();
-    pLetter();
-    pLetter();
-    iLetter();
+
   }
-  public static void pLetter(){
-    System.out.println(".______\n|   _  \\ \n|  |_)  |\n|   ___/\n|  |\n| _|");
-  }
-  public static void iLetter(){
-    System.out.println("    __ \n   |  |\n   |  |\n   |  |\n   |  |\n   |__|");
-  }
-  public static void sLetter(){
-    System.out.println("     _______.\n    /       |\n   |   (----`\n    \\   \\    \n.----)   |\n|_______/");
-  }
-  public static void mLetter(){
-    System.out.println(".___  ___.\n|   \\/   |\n|  \\  /  |\n|  |\\/|  |\n|  |  |  |\n|__|  |__|");
-  }
+  
 }
