b67e0198451bcf1cab55ba9aa5c3d8ae449ed573
namedrandom04 <zespychikenz@gmail.com>
2018-10-01 14:53:43 -0400

    added for loop 1

--- unit2/LoopLab.java
+++ unit2/LoopLab.java
@@ -1,6 +1,9 @@
 //Mert Albayrak looplab 10-01-18
 public class LoopLab {
-  public static void main(String[] args) {
-
+  public static void main(String[] args){
+    for (int i = 1;i <= 40 ; i++) {
+      System.out.print(i+" ");
+    }
+    System.out.println();
   }
 }
