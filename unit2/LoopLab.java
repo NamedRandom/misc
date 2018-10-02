754ea21841436934e9425259d60b8d67a1f4afc3
namedrandom04 <zespychikenz@gmail.com>
2018-10-02 09:49:14 -0400

    looplab2

--- unit2/LoopLab.java
+++
@@ -1,9 +0,0 @@
-//Mert Albayrak looplab 10-01-18
-public class LoopLab {
-  public static void main(String[] args){
-    for (int i = 1;i <= 40 ; i++) {
-      System.out.print(i+" ");
-    }
-    System.out.println();
-  }
-}
