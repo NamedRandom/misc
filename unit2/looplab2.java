9849d5e6050396d2639719bea629de3ac34f45b0
namedrandom04 <zespychikenz@gmail.com>
2018-10-03 12:45:23 -0400

    finished looplab2

---
+++ unit2/looplab2.java
@@ -0,0 +1,57 @@
+public class looplab2 {
+  public static void main(String[] args) {
+    System.out.print ("task 1: ");
+   for (int i=1; i<=40; i+=3) {
+      System.out.print (i+" ");
+      }
+    System.out.println();
+    System.out.println();
+    System.out.print("task 2: ");
+    for (int i=1; i<=20; i+=1){
+      System.out.print (i*i+" ");
+      }
+    System.out.println();
+    System.out.println();
+    System.out.println("task 3: ");
+    for (int i = 5; i > 0; i--) {
+        System.out.println("*"+i+"*");
+    }
+    System.out.println();
+    System.out.println();
+    System.out.println("task 4: ");
+    for (int i = 30; i > -1; i-=3) {
+        System.out.print("*"+i+" ");
+    }
+    System.out.println();
+    System.out.println();
+    System.out.println("task 5: ");
+    for (int i = 0; i <= 6; i++) {
+        System.out.println("**//\\\\**");
+    }
+    System.out.println();
+    System.out.println();
+    System.out.println("task 6: ");
+    for (int i = -30; i <= 0; i+=2) {
+        System.out.print(i+" ");
+    }
+    System.out.println();
+    System.out.println();
+    System.out.println("task 7: ");
+    for (int i = 2; i <= 256; i*=i) {
+        System.out.println(i);
+    }
+    System.out.println();
+    System.out.println();
+    System.out.println("task 8: ");
+    for (int i = 0; i <= 100; i+=10) {
+        System.out.print(i+"\" ");
+    }
+    System.out.println();
+    System.out.println();
+    System.out.println("task 9: ");
+    for (int i = 0; i <= 9; i++) {
+        System.out.print(i+"_"+i+"**");
+    }
+
+  }
+}
