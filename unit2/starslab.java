1a08c0a8b04e4d46993d718f2c28671f1b76276e
namedrandom04 <zespychikenz@gmail.com>
2018-10-07 19:15:47 -0400

    stars lab

---
+++ unit2/starslab.java
@@ -0,0 +1,41 @@
+//mert albayrak starslab 10-04-18
+public class starslab {
+  public static void main(String[] args) {
+    for (int i=1;i<5;i++)
+    System.out.print("*");
+    doubleBlank();
+    for (int i=1;i<5;i++){
+      for (int n = 1; n < 4; n++) {
+        for (Integer x = 1; x < 6; x++)
+          System.out.print("*");
+        System.out.println();
+      }
+      doubleBlank();
+    }
+    doubleBlank();
+
+  }
+  public static void doubleBlank() {
+    for (int i = 1; i < 3; i++)
+    System.out.println();
+  }
+  public static void boxStar() {
+    for (int i = 1; i < 6; i++) {
+      for (Integer i = 0; i < ; i++) {
+
+      }
+    }
+  }
+  public static void diagWidenStar() {
+
+  }
+  public static void Star() {
+
+  }
+  public static void boxStar() {
+
+  }
+  public static void boxStar() {
+
+  }
+}
