6a3230bffc05c17673961f7eb18bfb50dff71be3
namedrandom04 <zespychikenz@gmail.com>
2018-10-09 10:20:42 -0400

    test

--- unit2/starslab.java
+++ unit2/starslab.java
@@ -4,38 +4,43 @@ public class starslab {
     for (int i=1;i<5;i++)
     System.out.print("*");
     doubleBlank();
-    for (int i=1;i<5;i++){
-      for (int n = 1; n < 4; n++) {
-        for (Integer x = 1; x < 6; x++)
-          System.out.print("*");
-        System.out.println();
-      }
-      doubleBlank();
+    for (int i=1;i<=4;i++){
+      for (int n=1;n<=5;n++)
+        System.out.print("*");
+      System.out.println();
     }
     doubleBlank();
+    fourBoxes();
 
-  }
+    }
   public static void doubleBlank() {
     for (int i = 1; i < 3; i++)
     System.out.println();
   }
   public static void boxStar() {
     for (int i = 1; i < 6; i++) {
-      for (Integer i = 0; i < ; i++) {
+      for (Integer n = 0; n < 4; n++) {
 
       }
     }
   }
-  public static void diagWidenStar() {
+  public static void fourBoxes() {
+    for (int i=1;i<5;i++){
+      for (int n = 1; n < 4; n++){
+        for (Integer x = 1; x < 6; x++)
+          System.out.print("*");
+          System.out.println();
+        }
+    System.out.println();
+    }
+  }
+  public static void angledUpStar() {
 
   }
-  public static void Star() {
+  public static void angledDownStar() {
 
   }
-  public static void boxStar() {
-
-  }
-  public static void boxStar() {
+  public static void widerBoxStar() {
 
   }
 }
