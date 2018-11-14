8c059d6c997fd19c8b3c665e0bf2f28be3b5c3a1
namedrandom04 <zespychikenz@gmail.com>
2018-11-14 13:08:42 -0500

    Updated atomtest.java.

--- atomtest.java
+++ atomtest.java
@@ -1,5 +1,5 @@
 public class atomtest{
   public static void main(String[] args) {
-    System.out.print("  *    *    *    *\n * *  * *  * *  * *\n*   **   **   **   *\n * *  * *  * *  * *\n  *    *    *    *\n");
+    String s="hello this is a string"
   }
 }
