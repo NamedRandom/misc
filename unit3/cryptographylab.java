b140c3fe1581f1eb4b22bc29107df9f39e8e5b14
namedrandom04 <zespychikenz@gmail.com>
2018-11-14 10:11:45 -0500

    Updated cryptographylab.java.

--- unit3/cryptographylab.java
+++ unit3/cryptographylab.java
@@ -3,5 +3,7 @@ import java.util.Scanner;
 public class cryptographylab{
   public static void main(String[] args) {
     Scanner heyImAScanner = new Scanner(System.in);
+    System.out.print("Enter five words:\t")
+    String inputString = heyImAScanner.nextLine();
   }
 }
