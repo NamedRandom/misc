00cd2c68904bd29b5209955f171fad62fac5ed07
namedrandom04 <zespychikenz@gmail.com>
2018-11-14 09:39:15 -0500

    finished lab

--- unit3/methodswithparameters.java
+++ unit3/methodswithparameters.java
@@ -21,11 +21,39 @@ public class methodswithparameters {
    System.out.println(eq1);
    String eq2=getLineEquation(4.5, 3.1, 2.5);
    System.out.println(eq2);
-   String eq3=getLineEquation(0, -2, 3, 4);
+   String eq3=getLineEquation(3.1, 2.5, 7.6, 7);
    System.out.println(eq3);
   }
   public static void lattice (int x) {
     for (int i=1;i<=x;i++)
       System.out.print("  *    *    *    *\n * *  * *  * *  * *\n*   **   **   **   *\n * *  * *  * *  * *\n  *    *    *    *\n");
+  }
+  public static double inchesToFeet(double inches){
+    return(inches/12);
+  }
+  public static double feetToInches(double feet){
+    return(feet*12);
+  }
+  public static double feetToYards(double feet){
+    return(feet*(1/3.0));
+  }
+  public static double yardsToFeet(double yards){
+    return(yards*3);
+  }
+  public static double feetToMiles(double feet){
+    return(feet/5280.0);
+  }
+  public static double milesToFeet(double miles){
+    return(miles*5280);
+  }
+  public static String getLineEquation(double m,double b){
+    return("y="+m+"x+"+b);
+  }
+  public static String getLineEquation(double m,double x1,double y1){
+    return("y-"+y1+"="+m+"(x-"+x1+")");
+  }
+  public static String getLineEquation(double x1,double y1,double x2,double y2){
+    double m=(y1-y2)/(x1-x2*1.0);
+    return("y-"+y1+"="+m+"(x-"+x1+")");
   }
 }
