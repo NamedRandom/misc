2e8edb258336fa75ecf4b3bc844b4462720bb307
namedrandom04 <zespychikenz@gmail.com>
2018-11-19 14:23:52 -0500

    Updated cumulativesum.java.

--- unit3/cumulativesum.java
+++ unit3/cumulativesum.java
@@ -1,6 +1,16 @@
 //mert albayrak cumulativesum lab 11-19-18
+import java.util.Scanner;
 public class cumulativesum {
   public static void main(String[] args) {
-    
+    Scanner input=new Scanner(System.in);
+    System.out.print("Enter student name: ");
+    String name = input.nextLine();
+    System.out.println();
+    int average=0;
+    for (int i=1;i<=5;i++) {
+      System.out.print("Enter grade #"+i+": ");
+      average=input.nextInt()+average;
+    }
+    System.out.println(name+"'s average grade is "+average/5.0);
   }
 }
