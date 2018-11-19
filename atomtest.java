8718be7e3445c47a3b52913fd67a01509d857d51
namedrandom <mertalbayrak@gmail.com>
2018-11-18 21:03:06 -0500

    asdasd

--- atomtest.java
+++ atomtest.java
@@ -1,6 +1,7 @@
 public class atomtest{
   public static void main(String[] args) {
     String s=("hello this is a string");
-    System.out.println(s.replace( ,*));
+    System.out.println(s.replace(' ','*'));
+    String firstWordOfS=()
   }
 }
