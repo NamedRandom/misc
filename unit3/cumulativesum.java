26303ba70b585bc7b49a56b35629a59de14dc678
namedrandom04 <zespychikenz@gmail.com>
2018-11-19 14:28:01 -0500

    Updated cumulativesum.java.

--- unit3/cumulativesum.java
+++ unit3/cumulativesum.java
@@ -5,7 +5,7 @@ public class cumulativesum {
     Scanner input=new Scanner(System.in);
     int balance=0;
     for (int i=1;i<=10;i++) {
-      System.out.print("How much money to add for week "+i+": ");
+      System.out.print("Week "+i+": ");
       balance=input.nextInt()+balance;
       System.out.println(balance);
     }
