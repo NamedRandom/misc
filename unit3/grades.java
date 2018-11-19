b228bc63451dbcd44fe4be733a5266d79dc47cc2
namedrandom04 <zespychikenz@gmail.com>
2018-11-19 14:16:22 -0500

    Updated grades.java.

--- unit3/grades.java
+++ unit3/grades.java
@@ -2,6 +2,7 @@
 import java.util.Scanner;
 public class grades {
   public static void main(String[] args) {
-
+    Scanner input=new Scanner(System.in);
+    System.out.print("Enter student name: ");
   }
 }
