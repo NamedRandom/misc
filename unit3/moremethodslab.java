93e50eabd291a35720e5d2b3b0977fb7e4c72d72
namedrandom04 <zespychikenz@gmail.com>
2018-11-20 11:49:42 -0500

    Updated moremethodslab.java.

--- unit3/moremethodslab.java
+++ unit3/moremethodslab.java
@@ -3,8 +3,8 @@ public class moremethodslab {
 printPowersOf2(3);
   }
   public static void printPowersOf2(int goTo){
-    for (int i=0;i<=goTo;i++) {
+    for (int i=0;i<=goTo;i++)
       System.out.print((int)Math.pow(2,i)+" ");
-    }
+    System.out.println();
   }
 }
