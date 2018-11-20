bfb9da5fe583034797c2ba2ade9c06ab2a9e2702
namedrandom04 <zespychikenz@gmail.com>
2018-11-20 12:20:18 -0500

    Updated moremethodslab.java.

--- unit3/moremethodslab.java
+++ unit3/moremethodslab.java
@@ -2,10 +2,12 @@ public class moremethodslab {
   public static void main(String[] args) {
     printPowersOf2(3);
     printPowersOf2(10);
+
   }
   public static void printPowersOf2(int goTo){
     for (int i=0;i<=goTo;i++)
       System.out.print((int)Math.pow(2,i)+" ");
     System.out.println();
   }
+  public static void printWordNumTimes(String word,int )
 }
