ef3d06bd2c32ca05b57f3f212b5800a4a5279f9b
namedrandom <mertalbayrak@gmail.com>
2019-01-11 10:05:41 -0500

    split a rabbit

--- unit4/splittingaword.java
+++ unit4/splittingaword.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
 public class splittingaword {
   public static void main(String[] args) {
-    
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter word:");
+    printLetters(scanner.nextLine().replaceAll(" ","").trim());
+  }
+  public static void printLetters(String input){
+    for(int i=0;i<input.length()-1;i++) System.out.print(input.charAt(i)+", ");
+    System.out.print(input.charAt(input.length()-1)+"\n");
   }
 }
