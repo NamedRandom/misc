b4d0d76f1e4dd33e7a0ad5b25c9196f7cf6f33ed
namedrandom <mertalbayrak@gmail.com>
2019-01-15 14:23:31 -0500

    roman numerals are my nightmares

--- unit4/romannumeral.java
+++ unit4/romannumeral.java
@@ -2,27 +2,28 @@ import java.util.Scanner;
 public class romannumeral {
   public static void main(String[] args) {
     Scanner scanner=new Scanner(System.in);
-    System.out.print("Type number:\t");
+    System.out.print("Type number: ");
     int number=scanner.nextInt();
-    System.out.println(mNumeralMethod(number));
+    System.out.println("Roman Numeral: "+convertToRoman(number));
   }
-  public static String mNumeralMethod(int number){
-    int temp=number;
-    String output="";
-    boolean lessThan1000=false;
-    boolean lessThan500=false;
-    boolean lessThan100=false;
-    boolean lessThan50=false;
-    boolean lessThan10=false;
 
-    if(temp-1000<0) lessThan1000=true;
-    else if(temp-500<0)  lessThan500=true;
-      else if(temp-100<0)  lessThan100=true;
-        else if(temp-50<0)  lessThan50=true;
-          else if(temp-10<0)  lessThan10=true;
-    if(lessThan10) 10Method(number);
-    if()
-    return "number";
+  public static String convertToRoman(int number){
+    String output="";
+    for(int i=1;i<=number;i++) output+="I";
+    output=output.replaceAll("IIIII","V");
+    output=output.replaceAll("IIII","IV");
+    output=output.replaceAll("VV","X");
+    output=output.replaceAll("VIV","IX");
+    output=output.replaceAll("XXXXX","L");
+    output=output.replaceAll("XXXX","XL");
+    output=output.replaceAll("IIIII","V");
+    output=output.replaceAll("LXL","XC");
+    output=output.replaceAll("LL","C");
+    output=output.replaceAll("CCCCC","D");
+    output=output.replaceAll("CCCC","CD");
+    output=output.replaceAll("DCD","CM");
+    output=output.replaceAll("DD","M");
+    return output;
   }
 
 }
