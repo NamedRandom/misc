20e9da1222532aef09ab143a137a990f025517da
mert <mertalbayrak@gmail.com>
2019-01-29 12:17:28 -0500

    fixed sentinel

--- unit5/sentinel.java
+++ unit5/sentinel.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
 public class sentinel {
+    public static final int noVar=0;
+    public static final int yesVar=1;
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in); 
         int counter = 10;
         while(counter != 0){
             System.out.print("Are you bored yet? [Press 1 for Yes, 0 for No]: ");
             int answer = input.nextInt();
-            if(answer == 1) {
+            if(answer == yesVar) {
                 System.out.println("Finally!");
                 return;
             }
-            if(answer == 0) {
+            if(answer == noVar) {
                 if(counter != 1)System.out.println("Hmm..");
                 counter -= 1;
             }
