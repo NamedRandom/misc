5e8f205c0d663021992968c2e6707b916d5aca88
mert <mertalbayrak@gmail.com>
2019-02-26 14:47:41 -0500

    bruh.mp3

--- unit6/ArrayLibraryPractice.java
+++ unit6/ArrayLibraryPractice.java
@@ -1,6 +1,13 @@
 public class ArrayLibraryPractice {
-  public static void main(){
-
+  public static void main(String[] args){
+    int[] someArray = {31,42,64,12,53};
+    printArray(someArray);
+  }
+  public static void printArray(int[] someArray){
+      System.out.print("[");
+      for(int i:someArray)
+      System.out.print(i+",");
+      System.out.println("]");
   }
 
 }
