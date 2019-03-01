6a44757a83d3ffb8e1a4ae81d8010d734cc11875
mert <mertalbayrak@gmail.com>
2019-03-01 12:43:10 -0500

    finished arraylibprac

--- unit6/ArrayLibraryPractice.java
+++ unit6/ArrayLibraryPractice.java
@@ -1,15 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ArrayLibraryPractice {
   public static void main(String[] args){
-    int[] someArray = {31,42,64,12,53};
-    printArray(someArray);
-  }
-  public static void printArray(int[] someArray){
-      System.out.print("["); 
-      for(int i:someArray){
-        if(i==someArray.length-1) break;
-        System.out.print(i+",");
-        }
-        System.out.println("]");
-  }
+    
+      Scanner input = new Scanner(System.in);
+      int[] someArray = {31,42,64,12,53};
+      System.out.println("Original array: "+Arrays.toString(someArray));
+      System.out.print("Enter an integer from 0 to "+(someArray.length-1)+": ");
+      int storageVar = input.nextInt();
+      someArray[storageVar] = -1;
+      System.out.println("Replacing with -1: "+Arrays.toString(someArray));
+      System.out.print("Enter replacement for first number: ");
+      storageVar = input.nextInt();
+      someArray[0] = storageVar;
+      System.out.println("Replacing first: "+Arrays.toString(someArray));
+      System.out.print("Enter replacement for last number: ");
+      storageVar = input.nextInt();
+      someArray[someArray.length-1] = storageVar;
+      System.out.println("Replacing last: "+Arrays.toString(someArray));
 
+      
+  }
 }
