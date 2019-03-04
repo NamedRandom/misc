1f583c0d8897467a0c729721df7a74fd7261bb53
mert <mertalbayrak@gmail.com>
2019-03-04 14:16:42 -0500

    that was kinda easy

--- unit6/percenteven.java
+++ unit6/percenteven.java
@@ -9,19 +9,40 @@ public class percenteven {
        int[] myArray = new int[arrayLength];
        int[] evenArray = new int[arrayLength];
        double even=0;
+       boolean theresAZero=false;
 
-       for(int i:myArray){
+       for(int i=0;i<myArray.length;i++){
            System.out.print("Enter an integer: ");
            myArray[i] = input.nextInt();
            if(myArray[i]%2==0) {
                 evenArray[i]=myArray[i];
                 even++;
                 }
+           if(myArray[i]==0) theresAZero=true;
        }
         Arrays.sort(evenArray);
        System.out.println("There were "+even/arrayLength*100+"% of even numbers.");
-       System.out.println("Even values are: "+Arrays.toString(evenArray));
+       System.out.println("Even values are: "+Arrays.toString(trimArray(evenArray,theresAZero)));
         
         }
+
+    public static int[] trimArray(int[] input,boolean zero){
+        int count=0;
+
+        for(int i:input){
+            if(i!=0) count++;
+        }
+        if(zero) count++;
+        int[] output = new int[count];
+        int index=0;
+        for(int i:input){
+            if(i!=0){
+                output[index]=i;
+                index++;
+            }
+        }
+        return output;
+    }
+
     }
 
