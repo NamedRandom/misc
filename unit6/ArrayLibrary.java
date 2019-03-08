c541ffea29b9eaf2863aa8bd7effe212b7cdae58
mert <mertalbayrak@gmail.com>
2019-03-08 03:38:12 -0500

    finished arraylibtards and started 2 other labs

---
+++ unit6/ArrayLibrary.java
@@ -0,0 +1,78 @@
+import java.util.Arrays;
+import java.util.Random;
+public class ArrayLibrary {
+    //reverse an array
+    public static int[] reverse(int[] array) {
+        int[] output = new int[array.length];
+        int index=0;
+        for(int i=array.length-1;i>=0;i--){
+            output[index]=array[i];
+            index++;
+        }
+        return output;
+    }
+    
+    //shuffle an array
+    public static int[] shuffle(int[] array) {
+        Random rng = new Random();
+        for(int i=0;i<array.length;i++){
+           int swap;
+           int indexToMoveTo = rng.nextInt(array.length);
+           swap=array[indexToMoveTo];
+           array[indexToMoveTo]=array[i];
+           array[i]=swap;
+        }
+        return array;
+    }
+
+    //create a new array of size parameter
+    //filled with random numbers 1 - 20
+    public static int[] create(int size) {
+        Random rng = new Random();
+        int[] output = new int[size];
+        for(int i=0;i<size;i++){
+            output[i]=rng.nextInt(20)+1;
+        }
+        return output;
+    }
+
+    //returns how many times the array contains the value
+    public static int howManyOf(int[] array, int value) {
+       //replace the line below with your code
+       int counter=0;
+       for (int i:array) {
+            if(i==value){
+                counter++;
+            }
+        }
+        return counter;
+
+    }
+    
+    //returns true if the array contains the value
+    //returns false otherwise
+    public static boolean contains(int[] array, int value) {
+       //replace the line below with your code
+       return (howManyOf(array,value)>0);
+    }
+    
+
+    public static void main(String[] args) {
+       //test the methods here
+       int[] test1 = create(5);
+       int[] test2 = create(15);
+       int[] test3 = create(10);
+       int howManyOfNum = 8;
+       int containsVar1=3;
+       int containsVar2=7;
+        System.out.println("\nTesting reverse:\nOriginal: "+Arrays.toString(test1));
+        System.out.println("Reversed: "+Arrays.toString(reverse(test1))+"\n");
+        System.out.println("Testing shuffle:\nOriginal: "+Arrays.toString(test1));
+        System.out.println("Shuffled: "+Arrays.toString(shuffle(test1))+"\n");
+        System.out.println("Testing create:\n"+Arrays.toString(create(8))+"\n");
+        System.out.println("Testing howManyOf:\nArray: "+Arrays.toString(test2)+"\nhowManyOf "+howManyOfNum+": "+howManyOf(test2,howManyOfNum)+"\n");
+        System.out.println("Testing contains:\nArray: "+Arrays.toString(test3)+"\ncontains "+containsVar1+": "+contains(test3,containsVar1)+"\ncontains "+containsVar2+": "+contains(test3,containsVar2));
+       
+    }
+    
+}
---
+++ unit6/basicarrays2.java
@@ -0,0 +1,13 @@
+public class basicarrays2 {
+    public static void main(String[] args){
+        String[] names = {"Frew", "Manley"};
+        int[] favoriteNumbers = {0, 2, 8, 208};
+        double[] latitudes = {90.27, 90.30, 90.41};
+        char[] word = {'H', 'E', 'L', 'L', 'O'};
+
+            System.out.println("Length: "+names.length);
+            System.out.println("Last index: "+(names.length-1));
+            System.out.println("Last element: "+names[names.length-1]);
+
+    }
+}
