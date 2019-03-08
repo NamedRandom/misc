af865a9e63714b5371db25b53ef62ee77184e6d0
mert <mertalbayrak@gmail.com>
2019-03-08 14:11:13 -0500

    thats a lot of println();

--- unit6/basicarrays2.java
+++ unit6/basicarrays2.java
@@ -8,6 +8,23 @@ public class basicarrays2 {
             System.out.println("Length: "+names.length);
             System.out.println("Last index: "+(names.length-1));
             System.out.println("Last element: "+names[names.length-1]);
+            System.out.println("Second to last element: "+names[names.length-2]);
+            System.out.println();
+            System.out.println("Length: "+word.length);
+            System.out.println("Last index: "+(word.length-1));
+            System.out.println("Last element: "+word[word.length-1]);
+            System.out.println("Second to last element: "+word[word.length-2]);
+            System.out.println();
+            System.out.println("Length: "+latitudes.length);
+            System.out.println("Last index: "+(latitudes.length-1));
+            System.out.println("Last element: "+latitudes[latitudes.length-1]);
+            System.out.println("Second to last element: "+latitudes[latitudes.length-2]);
+            System.out.println();
+            System.out.println("Length: "+favoriteNumbers.length);
+            System.out.println("Last index: "+(favoriteNumbers.length-1));
+            System.out.println("Last element: "+favoriteNumbers[favoriteNumbers.length-1]);
+            System.out.println("Second to last element: "+favoriteNumbers[favoriteNumbers.length-2]);
+
 
     }
 }
