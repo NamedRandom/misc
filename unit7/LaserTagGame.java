5fa7b399396684afba034c8bdbe73d7b894d1886
asphyxia <mertalbayrak@gmail.com>
2019-04-12 14:03:21 -0400

    uhh

--- unit7/LaserTagGame.java
+++ unit7/LaserTagGame.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class LaserTagGame {
     public static void main(String[] args){
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("How many players?");
+        int playerCount = input.nextInt();
+        LaserTagPlayer[] players = new LaserTagPlayer[playerCount];
+        for(int i=0;i<players.length;i++){
+            System.out.print("Enter player "+(i+1)+"'s name: ");
+            String name = input.nextLine();
+            players[i] = new LaserTagPlayer(name);
+        }
 
     }
 }
