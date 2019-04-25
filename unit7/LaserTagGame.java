cdbc769fe35d269fd90f136eb6865b84acfe6a66
asphyxia <asphyxia>
2019-04-25 07:35:34 -0400

    epicepicepic

--- unit7/LaserTagGame.java
+++ unit7/LaserTagGame.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class LaserTagGame {
     public static void main(String[] args){
@@ -9,9 +10,36 @@ public class LaserTagGame {
         LaserTagPlayer[] players = new LaserTagPlayer[playerCount];
         for(int i=0;i<players.length;i++){
             System.out.print("Enter player "+(i+1)+"'s name: ");
-            String name = input.nextLine();
+            String name = input.next();
             players[i] = new LaserTagPlayer(name);
         }
+        boolean[] gameTracker = new boolean[players.length];
+        Arrays.fill(gameTracker,true);
+	boolean gameWon=false;
+	while(!gameWon){
+        
+        playGame(players,gameTracker);
+        gameWon=winCheck(gameTracker);
 
+	}
+    
+    System.out.println("Congrats! "+players[0]+" wins!");
+    
+    }
+    public static void playGame(LaserTagPlayer[] players, boolean[] tracker){
+
+    }
+    public static boolean winCheck(boolean[] tracker){
+        if(tracker.length==1) return true;
+        shrinkArray(tracker);
+        return false;
+    }
+    public static boolean[] shrinkArray(boolean[] arr){
+        int counter=0;
+        for(boolean b:arr)
+            if(b) counter++;
+        boolean[] arrOut = new boolean[counter];
+        Arrays.fill(arrOut,true);
+        return arrOut;
     }
 }
