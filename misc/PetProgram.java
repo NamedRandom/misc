86e378d2735dea8b6d0b963c90f7657f3cca107c
asphyxia <asphyxia>
2019-04-28 23:26:16 -0400

    fixed a silly mistake

--- misc/PetProgram.java
+++ misc/PetProgram.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 public class PetProgram {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
@@ -6,11 +7,11 @@ public class PetProgram {
         Pet[] pets = new Pet[5];
         
         for(int i=0;i<pets.length;i++){
-            System.out.print("Name of pet "+i);
+            System.out.print("Name of pet "+(i+1)+" ");
             String name = input.next();
-            System.out.print("Age of pet "+i+" (years)");
+            System.out.print("Age of pet "+(i+1)+" (years) ");
             int age = input.nextInt();
-            System.out.print("Weight of pet "+i+" (pounds)");
+            System.out.print("Weight of pet "+(i+1)+" (pounds) ");
             double weight = input.nextDouble();
 
             pets[i] = new Pet(name,age,weight); 
@@ -40,12 +41,16 @@ public class PetProgram {
     }
     public static String[] findValues(Pet[] arr){
         
+        String[] output= new String[4];
+        
         double smallest=arr[0].getWeight();
         double largest=arr[0].getWeight();
         int oldest=arr[0].getAge();
         int youngest=arr[0].getAge();
-
-        String[] output= new String[4];
+        output[0] = arr[0].getName();
+        output[1] = arr[0].getName();
+        output[2] = arr[0].getName();
+        output[3] = arr[0].getName();
         
         //main loop to find values
         for(Pet p : arr){
@@ -55,12 +60,12 @@ public class PetProgram {
                 smallest = p.getWeight();
             }
             //find largest
-            if(p.getWeight()<largest){
+            if(p.getWeight()>largest){
                 output[1] = p.getName();
                 largest = p.getWeight();
             }
             //find oldest
-            if(p.getAge()<oldest){
+            if(p.getAge()>oldest){
                 output[2] = p.getName();
                 oldest = p.getAge();
             }
