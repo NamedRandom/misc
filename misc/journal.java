5eba5ea9812b342b04e1bdb683faad6d3ef5e956
asphyxia <asphyxia>
2019-05-05 09:10:51 -0400

    waid

---
+++ misc/journal.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class journal {
+    public static void main(String[] args){
+        
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.println("Welcome, what is your family name?: ");
+        String family = input.next();
+
+        System.out.println("\n\n\nYou are a SERF of the "+family+" family.");
+
+    }
+}
